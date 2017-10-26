939852705c479274cbf5fb138ecf05423ce0c4c5
scott.mccain@gmail.com <mccains@pacden.com>
2017-10-25 17:07:24 -0700

    Added JSON parser

--- src/app/controllers/main-controller/index.js
+++ src/app/controllers/main-controller/index.js
@@ -1,13 +1,16 @@
 export default class {
-    constructor($location, $http, $sce, $timeout) {
+    constructor($location, $http, $sce, $timeout, $uibModal) {
         this.$location = $location;
         this.$http = $http;
 	    this.$sce = $sce;
         this.$timeout = $timeout;
+        this.$uibModal = $uibModal;
 
         this.pdfText = "";
         this.pdfTitle = "";
         this.content = null;
+        this.jsonText = "";
+        this.jsonObject = null;
     }
 
     submit() {
@@ -24,5 +27,30 @@ export default class {
             this.active = 1;
             this.content = this.$sce.trustAsResourceUrl(fileURL);
         });		
+    }
+
+    convertJson() {
+        this.jsonObject = JSON.parse(this.jsonText);
+    }
+
+    showJsonHelper() {
+        this.$uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title-top',
+            ariaDescribedBy: 'modal-body-top',
+            template: require('../../templates/modals/jsonHelper'),
+            size: 'lg',
+            controllerAs: 'vm',
+            controller: class  {
+                constructor() {
+                    this.jsonText = '';
+                    this.jsonObject = {};
+                }
+              
+                convertJson() {
+                  this.jsonObject = JSON.parse(this.jsonText)
+                }
+            }
+          });
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ require('angular-ui-bootstrap');
 require('jquery/dist/jquery');
 
 import '../../node_modules/bootstrap/dist/css/bootstrap';
+import '../../node_modules/bootstrap/dist/css/bootstrap-theme';
 import '../../node_modules/font-awesome/css/font-awesome.css';
 import './styles/app.css';
 
