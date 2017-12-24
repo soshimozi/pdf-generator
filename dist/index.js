1f9b8b53f3b68ac6fe0a850ab0f50578b2f69083
Scott McCain <scott.mccain@gmail.com>
2017-12-24 15:39:04 -0800

    Added parameterized port

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,9 @@ const pdf = require('html-pdf');
 const path = require('path');
 const logger = require('morgan');
 
+
+let port = process.env.NODE_PORT || 3000;
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -77,6 +80,6 @@ app.use(function(err, req, res, next) {
     }
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!')
   });
---
+++ dist/startApp.sh
@@ -0,0 +1,3 @@
+export NODE_ENV=production
+export NODE_PORT=8080
+node /pdf-generator/index.js&
