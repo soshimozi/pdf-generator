8dd93abf5dfe1a00e7037f4a28c40d87d7a825e3
Scott McCain <scott.mccain@gmail.com>
2017-12-28 22:31:31 -0800

    Fixed startup script

--- dist/startApp.sh
+++ dist/startApp.sh
@@ -1,2 +1,2 @@
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -               u ubuntu --hp /home/ubuntu
-pm2 start ecosystem.config.js
+sudo pm2 start ecosystem.config.js
