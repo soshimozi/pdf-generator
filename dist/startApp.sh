a38a05709c505722499e620493ff4259b3149c2f
Scott McCain <scott.mccain@gmail.com>
2017-12-29 14:38:56 -0800

    Trying to fix pm2 startup

--- dist/startApp.sh
+++ dist/startApp.sh
@@ -1,2 +1,2 @@
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -               u ubuntu --hp /home/ubuntu
-sudo pm2 start ecosystem.config.js
+pm2 start ecosystem.config.js
